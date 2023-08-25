7df01913288a657dfb8c004396e8653460f02fb3
Jhorlan-Ortega <124930448+Jhorlan-Ortega@users.noreply.github.com>
2023-08-25 15:07:46 -0500

    Update POOER14.java

--- POOER14.java
+++ POOER14.java
@@ -3,7 +3,7 @@ package Ejersicios_Trabajao1_POO;
 import javax.swing.JOptionPane;
 
 public class POOER14 {
-
+//Ejercicio 14
 	public static void main(String[] args) {
 		
 		int numero=Integer.parseInt(JOptionPane.showInputDialog("Ingrese un numero entero: "));
