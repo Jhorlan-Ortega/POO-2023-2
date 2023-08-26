2f5f9e820214f32564fa4a9365d7c40cc5e11e6b
Jhorlan-Ortega <124930448+Jhorlan-Ortega@users.noreply.github.com>
2023-08-26 11:14:04 -0500

    Add files via upload

---
+++ POOEP_12.java
@@ -0,0 +1,41 @@
+package Ejersicios_Trabajao1_POO;
+
+import javax.swing.JOptionPane;
+
+public class POOEP_12 {
+
+	public static void main(String[] args) {
+		double rete_fuente;
+		double sa_bruto;
+		final int dias;
+		final double precio;
+		double re_fuente;
+		double sa_neto;
+		
+	int pregunta=Integer.parseInt(JOptionPane.showInputDialog("Desea realizar el ejerciso con: \n1. Valores por defecto: \n2.Valores por consola: "));
+	if (pregunta==1) {
+		precio=5000;
+		dias=48;
+		rete_fuente=(12.5)/100;
+		sa_bruto= dias*precio;
+		re_fuente=sa_bruto*rete_fuente;
+		sa_neto=sa_bruto-re_fuente;
+		System.out.println("El salario bruto: "+sa_bruto+"\nLa retencion fuente: "+re_fuente+"\nSalario neto del Trabajador: "+sa_neto);
+	}
+	else {
+		dias=Integer.parseInt(JOptionPane.showInputDialog("Ingrese dias trabajados: "));
+		precio=Double.parseDouble(JOptionPane.showInputDialog("Ingrese valor de hora trabajada: "));
+		rete_fuente=Double.parseDouble(JOptionPane.showInputDialog("Ingrese retencion en la fuente en porcentaje (%): "));
+		rete_fuente/=100;
+		sa_bruto= dias*precio;
+		re_fuente=sa_bruto*rete_fuente;
+		sa_neto=sa_bruto-re_fuente;
+		System.out.println("El salario bruto: "+sa_bruto+"\nLa retencion fuente: "+re_fuente+"\nSalario neto del Trabajador: "+sa_neto);
+		
+	}
+	
+	
+
+	}
+
+}
---
+++ POOEP_17.java
@@ -0,0 +1,28 @@
+package Ejersicios_Trabajao1_POO;
+import javax.swing.*;
+public class POOEP_17 {
+
+	public static void main(String[] args) {
+	String solicitud;
+	double radio=Double.parseDouble(JOptionPane.showInputDialog("Ingrese el radio del circulo en cm: "));
+	double area=(Math.PI)*(Math.pow(radio, 2));
+	double circunferencia=2*Math.PI*radio;
+	double volumen;
+	System.out.print("El area del circulo es: ");
+	System.out.printf("%1.2f",+area);
+	System.out.println(" cm^2");
+	System.out.print("La circunferencia del circulo es: ");
+	System.out.printf("%1.2f",circunferencia);
+	System.out.println(" cm");
+	solicitud=JOptionPane.showInputDialog("¿Quieres conocer tambien el volumen de una\nesfera de radio "+(int)radio+" cm ?:\nSi\nNo");
+	if (solicitud.equalsIgnoreCase("si")){
+		volumen=((Math.PI)*(Math.pow(radio, 3)*(4))/3);
+		System.out.print("El volumen de una esfera con radio "+radio+" es: ");
+		System.out.printf("%1.2f",volumen);
+		System.out.println(" cm^3");
+	}
+	
+	
+	}
+
+}
